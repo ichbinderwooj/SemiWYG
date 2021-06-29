5a854ea6315d7cbff998ff352e63c7172a37705e
Wooj <sonwoojin161@gmail.com>
2021-06-29 09:55:26 +0200

    initial commit

---
+++ src/main.js
@@ -0,0 +1,30 @@
+const {app, BrowserWindow} = require("electron");
+
+let window;
+
+function createWindow() {
+    window = new BrowserWindow({
+        width: 800,
+        height: 800
+    });
+
+    window.loadURL(`file://${__dirname}/html/index.html`);
+
+    window.on("closed", function () {
+        win = null;
+    });
+}
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", function () {
+    if (process.platform !== "darwin") {
+        app.quit();
+    }
+});
+
+app.on("activate", function () {
+    if (win === "null") {
+        createWindow();
+    }
+});
